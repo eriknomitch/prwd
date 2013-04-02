7d9bdb1f2acd8c85d0a990541cf1b35f8aa691bb
Erik Nomitch <erik@nomitch.com>
2013-04-02 04:57:41 -0700

    support for multiple directory listing, adding, and cd-ing

--- persistent-working-directory.zsh
+++ persistent-working-directory.zsh
@@ -6,12 +6,24 @@ WORKING_DIRECTORY_SYMBOLIC_LINK=$HOME/.wd
 
 function lw()
 {
-  test -e $WORKING_DIRECTORY_FILE && cat $WORKING_DIRECTORY_FILE
+  test -e $WORKING_DIRECTORY_FILE && cat $WORKING_DIRECTORY_FILE | sort
 }
 
 function sw()
 {
-  echo $PWD > $WORKING_DIRECTORY_FILE
+  _target=$1
+
+  # Default to 0
+  if [[ -z $_target ]] ; then
+    echo "setting target to 0"
+    _target=0
+  fi
+
+  # Destroy the target if it exists
+  sed -i "/^$_target:.*$/d" $WORKING_DIRECTORY_FILE
+
+  # Add it to the working directory file
+  echo "$_target:$PWD" >> $WORKING_DIRECTORY_FILE
 
   # ensure symbolic link is removed
   test -L $WORKING_DIRECTORY_SYMBOLIC_LINK && rm $WORKING_DIRECTORY_SYMBOLIC_LINK
@@ -24,8 +36,16 @@ function sw()
 
 function gw()
 {
+  _target=$1
+
+  # Default to 0
+  if [[ -z $_target ]] ; then
+    _target=0
+  fi
+
   if [[ -e $WORKING_DIRECTORY_FILE ]] ; then
     clear
-    cd `cat $WORKING_DIRECTORY_FILE`
+    cd `cat $WORKING_DIRECTORY_FILE | grep -E "^$_target:" | sed "s/^$_target://"`
   fi
 }
+
