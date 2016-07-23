f3b8fcec21d0b3e22443e142e95ea9fe5cc9ce89
Erik Nomitch <enomitch@gmail.com>
2016-07-23 13:53:57 -0500

    gw without a directory set goes home

--- persistent-working-directory.zsh
+++ persistent-working-directory.zsh
@@ -122,8 +122,9 @@ function gw()
     clear
 
     if [[ -z $_directory ]] ; then
-      echo "$0: Working directory not set for target=$_target workspace=$_workspace. Staying in this directory."
-      pwd
+      echo "$0: Working directory not set for target=$_target workspace=$_workspace. Changing directory to '~/'."
+
+      cd
 
       if ( `(command -v g >/dev/null 2>&1)` ) ; then
         g
@@ -133,7 +134,8 @@ function gw()
       cd $_directory
     fi
   else
-    echo "$0: No directories are set. Staying in this directory."
+    echo "$0: No directories are set. Changing directory to '~/'."
+    cd
     return
   fi
 }
